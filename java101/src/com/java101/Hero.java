714f627bd2d33b28b8ca68dea6b99abe04600848
Anna Modrzejewska <modrzejewskaa@gmail.com>
2016-10-16 16:20:34 +0200

    Changes after comments 1

--- java101/src/com/java101/Hero.java
+++ java101/src/com/java101/Hero.java
@@ -1,45 +1,45 @@
-
 package com.java101;
 
 public class Hero {
-	
 	private String name;
 	private int power;
 	private int skill;
 	private int vitality;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getPower() {
 		return power;
 	}
+
 	public void setPower(int power) {
 		this.power = power;
 	}
+
 	public int getSkill() {
 		return skill;
 	}
+
 	public void setSkill(int skill) {
 		this.skill = skill;
 	}
+
 	public int getVitality() {
 		return vitality;
 	}
+
 	public void setVitality(int vitality) {
 		this.vitality = vitality;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Hero [name=" + name + ", power=" + power + ", skill=" + skill + ", vitality=" + vitality + "]";
 	}
-
-	
-	
-	
-
 }
---
+++ java101/src/com/java101/HeroService.java
@@ -0,0 +1,52 @@
+package com.java101;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HeroService {
+
+	public static void main(String[] params) {
+		List<Hero> listaBohaterow = new ArrayList<>();
+
+		Scanner scan = new Scanner(System.in);
+		int number;
+
+		do {
+			System.out.println("Wybierz jedn¹ opcjê: \nWprowadz bohatera - 1 \nNic nie rób - 0");
+			number = scan.nextInt();
+
+			switch (number) {
+			case 1:
+				System.out.println("Podaj imiê bohatera");
+				String name = scan.next();
+				System.out.println("Podaj si³ê bohatera");
+				int sila = scan.nextInt();
+				System.out.println("Podaj zrêcznoœæ bohatera");
+				int zrecznosc = scan.nextInt();
+				System.out.println("Podaj witalnoœæ bohatera");
+				int witalnosc = scan.nextInt();
+				Hero bohater = new Hero();
+				bohater.setName(name);
+				bohater.setPower(sila);
+				bohater.setSkill(zrecznosc);
+				bohater.setVitality(witalnosc);
+				listaBohaterow.add(bohater);
+				System.out.println("Podany bohater: " + "\nImiê: " + name + ", Si³a: " + sila + ", Zrêcznoœæ: "
+						+ zrecznosc + ", Witalnoœæ: " + witalnosc);
+				break;
+			case 0:
+				break;
+			default:
+				break;
+			}
+		} while (number != 0);
+
+		if (listaBohaterow != null && listaBohaterow.size() != 0) {
+			for (int i = 0; i < listaBohaterow.size(); i++) {
+				System.out.println(listaBohaterow.get(i));
+			}
+		}
+	}
+
+}
