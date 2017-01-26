67dc814f0d7192d0eb5099c69e1f82458dea23b6
Anna Modrzejewska <modrzejewskaa@gmail.com>
2017-01-26 21:36:09 +0100

    Adding spring annotations: EnableAutoConfiguration, SpringBootApplication

--- java101/src/com/java101/Application.java
+++ java101/src/com/java101/Application.java
@@ -3,7 +3,11 @@ package com.java101;
 import java.io.IOException;
 
 import org.json.simple.parser.ParseException;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 	
 	public static void main(String[] params) throws ParseException, IOException {
